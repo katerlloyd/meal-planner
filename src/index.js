edbc826547bc771215b52d5c47d5b4ff1c89ed5f
katereclark <cla16022@byui.edu>
2023-06-10 16:41:43 -0600

    Reworked webpack.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initializeApp } from "firebase-admin/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,12 +18,23 @@ const firebaseConfig = {
   storageBucket: "practice-df7f3.appspot.com",
   messagingSenderId: "518821478472",
   appId: "1:518821478472:web:f64e5e9f4924d8d5c8d87d",
-  measurementId: "G-HMYC6RV32V"
+  measurementId: "G-HMYC6RV32V",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+// const db = getFirestore(firebaseApp);
+// const analytics = getAnalytics(firebaseApp);
+
+// Detect auth state
+onAuthStateChanged(auth, (user) => {
+  if (user != null) {
+    console.log(`${user} is logged in`);
+  } else {
+    console.log("No user");
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
